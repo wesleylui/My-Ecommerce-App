c778d2e7029ef51c5d26ea4920f11a649056ebe3
wesleylui <wesley.lui.wl@gmail.com>
2024-03-23 17:32:37 -0600

    starting connecting files App calls Homepage which calls homeMainSection which has header, main section, and footer get code from prev assignment for the homepage sections

--- my-ecommerce-app/src/component/Homepage.js
+++ my-ecommerce-app/src/component/Homepage.js
@@ -1,14 +1,16 @@
 import React from "react";
-import Header from "./Header";
-import HomeMainSection from "./HomeMainSection";
-import Footer from "./Footer";
+import Header from "./header.js";
+import HomeMainSection from "./homeMainSection.js";
+import Footer from "./footer.js";
+// import reviews from "../data/reviews";
 
 function Homepage() {
   return (
     <div>
+      From Homepage.js (Child)
       <Header />
-      <HomeMainSection />
-      <Footer />
+      {/* <HomeMainSection /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ my-ecommerce-app/src/component/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header class="header">
+            <div class="logo">
+                <img src="my-ecommerce-app\public\images\logo.png" alt="Company Logo"></img>
+            </div>
+            <div class="company-name">
+                Company Name
+            </div>
+        </header>
+    );
+}
+
+export default Header;
