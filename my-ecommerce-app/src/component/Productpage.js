cc127dd2efafc8f8152f4cf9ec84736d41469382
wesleylui <wesley.lui.wl@gmail.com>
2024-03-23 23:20:39 -0600

    create Productpage.js skeleton

---
+++ my-ecommerce-app/src/component/Productpage.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./header.js";
+import Footer from "./footer.js";
+import ProductList from "./ProductList.js";
+import Cart from "./Cart.js";
+
+
+function Productpage() {
+  return (
+    <div className="product-page">
+      <Header />
+      <table>
+        <tr>
+            <td><ProductList /></td>
+            <td style={{verticalAlign: 'top'}}><Cart /></td>
+        </tr>
+      </table>
+      <Footer />
+    </div>
+  );
+}
+
+export default Productpage;
+
