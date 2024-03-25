0193ee6429016d97de58b2485c7c62b402d4769b
wesleylui <wesley.lui.wl@gmail.com>
2024-03-25 16:47:48 -0600

    change Productpage

--- my-ecommerce-app/src/component/Productpage.js
+++ my-ecommerce-app/src/component/Productpage.js
@@ -1,20 +1,78 @@
-import React from "react";
+import React, { createContext, useState, useEffect } from "react";
 import Header from "./header.js";
 import Footer from "./footer.js";
-import ProductList from "./ProductList.js";
-import Cart from "./Cart.js";
+import ProductList from "./ProductList";
+import Cart from "./Cart";
+
+export const CartItemsContext = createContext(null);
 
 function Productpage() {
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  useEffect(() => {
+    const itemsInLocal = JSON.parse(localStorage.getItem("cartItems") || "[]");
+    setCartItems(itemsInLocal);
+
+    let calculatedTotalPrice = 0;
+    for (let i = 0; i < itemsInLocal.length; i++) {
+      calculatedTotalPrice += itemsInLocal[i].total;
+    }
+    setTotalPrice(calculatedTotalPrice);
+  }, []);
+
+  const handleCartAdd = (childData) => {
+    let inCart = false;
+    for (let i = 0; i < cartItems.length; i++) {
+      if (childData.id === cartItems[i].id) {
+        cartItems[i].quantity += 1;
+        cartItems[i].total += childData.price;
+        setTotalPrice(totalPrice + childData.price);
+        inCart = true;
+      }
+    }
+    if (cartItems.length === 0 || inCart === false) {
+      childData.quantity = 1;
+      childData.total = childData.price;
+      cartItems.push(childData);
+      setTotalPrice(totalPrice + childData.price);
+    }
+    setCartItems(cartItems);
+    const string = JSON.stringify(cartItems);
+    localStorage.setItem("cartItems", string);
+    localStorage.setItem("totalPrice", totalPrice);
+  };
+
+  const handleCartRemove = (childData) => {
+    for (let i = 0; i < cartItems.length; i++) {
+      if (childData.id === cartItems[i].id) {
+        cartItems[i].quantity -= 1;
+        cartItems[i].total -= cartItems[i].price;
+        setTotalPrice(totalPrice - cartItems[i].price);
+        if (cartItems[i].quantity === 0) {
+          cartItems.splice(i, 1);
+        }
+        break;
+      }
+    }
+    setCartItems(cartItems);
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
+
   return (
     <div className="product-page">
       <Header />
-      <table>
+      <table style={{ width: "100%" }}>
         <tr>
-          <td>
-            <ProductList />
+          <td style={{ verticalAlign: "top" }}>
+            <ProductList handleCartAdd={handleCartAdd} />
           </td>
           <td style={{ verticalAlign: "top" }}>
-            <Cart />
+            <Cart
+              handleCartRemove={handleCartRemove}
+              totalPrice={totalPrice}
+              cartItems={cartItems}
+            />
           </td>
         </tr>
       </table>
