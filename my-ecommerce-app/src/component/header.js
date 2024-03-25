cdeb5748b45418384c1d5656b2a2b368234f94bd
MarcusLieu <marcus.lieu4@gmail.com>
2024-03-25 16:12:33 -0600

    fixed logo

--- my-ecommerce-app/src/component/header.js
+++ my-ecommerce-app/src/component/header.js
@@ -6,14 +6,13 @@ function Header() {
         <div>
             <header class="header">
                 <div class="logo">
-                    <img src="./my-ecommerce-app/public/images/logo.png" alt="Company Logo"></img>
+                    <img src={process.env.PUBLIC_URL + "images/logo.png"} alt="Company Logo"></img>
                     {/*<!-- TODO: fix image path -->*/}
                 </div>
                 <div class="company-name">
                     Company Name
                 </div>
             </header>
-
             <nav>
                 <a href="/" class="nav-link">Home</a>
                 <a href="products.html" class="nav-link">Products</a> 
