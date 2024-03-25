731b06c4bcf772879311c3c419740a79ba2d730f
wesleylui <wesley.lui.wl@gmail.com>
2024-03-25 13:46:33 -0600

    fix typo

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./component/Homepage.js";
-import ProductPage from "./component/ProductPage.js";
+import ProductPage from "./component/Productpage.js";
 
 
 function App() {
